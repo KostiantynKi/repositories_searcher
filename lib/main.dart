6e3bae6547dde75e8d588bcd93af09a27ea9d276
Kostiantyn <kostiantyn.kirkach@gmail.com>
2024-03-15 22:02:45 +0100

    added fonts

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:repositories_searcher/bloc/cubit/searcher_cubit.dart';
+import 'package:repositories_searcher/resource/app_fonts.dart';
 import 'package:repositories_searcher/screens/search_screen.dart';
+import 'package:repositories_searcher/theme/colors/app_colors.dart';
 
 void main() {
   runApp(MyApp());
@@ -14,7 +16,38 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       title: 'GitHub Search',
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        textTheme: const TextTheme(
+          displayLarge: TextStyle(
+            fontFamily: 'Raleway',
+            fontSize: 16.0,
+            fontWeight: AppFonts.bold,
+            color: AppColors.textPrimary,
+            letterSpacing: 0.0,
+          ),
+          bodyMedium: TextStyle(
+            fontFamily: 'Raleway',
+            fontSize: 14.0,
+            fontWeight: AppFonts.regular,
+            color: AppColors.textPlaceholder,
+            letterSpacing: 0.0,
+          ),
+          displayMedium: TextStyle(
+            fontFamily: 'Raleway',
+            fontSize: 14.0,
+            fontWeight: AppFonts.regular,
+            color: AppColors.textPrimary,
+            letterSpacing: 0.0,
+          ),
+          labelLarge: TextStyle(
+            fontFamily: 'Raleway',
+            fontSize: 16.0,
+            fontWeight: AppFonts.regular,
+            color: AppColors.accentPrimary,
+            letterSpacing: 0.0,
+          ),
+        ),
+        scaffoldBackgroundColor: AppColors.backgroundMain,
+        // primarySwatch: AppColors.accentPrimary,
       ),
       home: BlocProvider(
         create: (context) => SearcherCubit(),
---
+++ lib/resource/app_fonts.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppFonts {
+  static const String fontFamilySegoeUi = 'Raleway';
+
+  static const FontWeight regular = FontWeight.w400;
+  static const FontWeight bold = FontWeight.w600;
+}
